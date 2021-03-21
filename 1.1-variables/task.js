9a634116676230383f70229e317618dca9e66a9c
Maria <mariaes2016@yandex.ru>
2021-03-21 11:52:13 +0300

    task fixed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -19,9 +19,8 @@ function averageMark(a, g, p) {
 }
 
 function sayHello(userName) {
-	let myName = Мария;
 	let message;
-	message = (`Привет, мир! меня зовут ${myName}`);
+	message = (`Привет, мир! меня зовут ${userName}`);
     // реализуйте здесь задание №2
     
 	return message // ваш ответ;
@@ -31,7 +30,7 @@ function calculateFormula() {
     let x = 2;
     let y = 22;
     let z = 0;
-    let result = ('x "*" y "+" 5 "*" z "+" x "-" 1');
+    let result = (x * y + 5 * z + x - 1);
 	// реализуйте здесь задание №3
     
     return result// ваш ответ;
