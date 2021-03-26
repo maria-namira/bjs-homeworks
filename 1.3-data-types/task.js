852143cc2ad2756b5e4ab021a961b12ba6764d7d
Maria <mariaes2016@yandex.ru>
2021-03-26 17:35:11 +0300

    data types completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
+	if (isNaN(percent)) {
+        return `Параметр 'Процентная ставка' содержит неправильное значение ${percent}.`;
+    }
+    else if (isNaN(contribution)) {
+        return `Параметр 'Первоначальный взнос' содержит неправильное значение ${contribution}.`;
+    }
+    else if (isNaN(amount)) {
+        return `Параметр 'Сумма кредита' содержит неправильное значение ${amount}.`;
+    }
+    let percentRate = (percent / 100) / 12; // 1/12 процентной ставки
+    let period = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); // количество месяцев
+    let currentPay = (amount - contribution) * (percentRate + percentRate / ((Math.pow((1 + percentRate), period)) - 1)); // ежемесячный платеж
+    let totalAmount = amount + (currentPay * period - amount);
+    return Number(totalAmount.toFixed(2));
     // код для задачи №1 писать здесь
     // return totalAmount;
 }
 
 function getGreeting(name) {
+	 let greeting = `Привет, мир! Меня зовут ${(name === undefined || name === null || name === '') ? 'Аноним' : name}`;
+    return greeting;
+    console.log(greeting);
     // код для задачи №2 писать здесь
     // return greeting;
 }
